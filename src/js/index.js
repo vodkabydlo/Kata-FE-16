43da80dc0f49b0b0d72c19533816cfaca62a8c63
irisrumtub <tvburis@gmail.com>
2022-09-25 15:54:40 +0300

    sync

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,22 @@
 require("../scss/style.scss");
 require("../js/event_handlers");
+require("./swiper.js")
+// const swiper = new Swiper('.swiper', {
+//     speed: 400,
+//     spaceBetween: 100,
+//   });
+const swiper = new Swiper('.swiper', {
+  breakpoints: {
+      768: {
+          slidesPerView: 2,
+      },
+      1440: {
+          slidesPerView: 3
+      },
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 1,
+});
